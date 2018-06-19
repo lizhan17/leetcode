d626421497f9f92080d7893b82aff0dfda2e0243
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-06-18 17:10:34 -0700

    Add files via upload

---
+++ 78_subsets.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res= [[]] # just include special case []
+        
+        length_of_nums = len(nums) 
+        for num in range(1,2**length_of_nums):   # brute force all the possiblity
+            
+            count_arr= []
+             
+            while(num>0):
+                t= num%2
+                count_arr.append(t)
+                num = int((num-t)/2)
+            
+            tmp_list=[]
+            for h in range(0,length_of_nums):  # for each count number we get a distinct array represent the situation
+                 # from the situation, we get a tmp list which is a subset
+                if(h<len(count_arr) and count_arr[h]==1):           
+                    tmp_list.append(nums[h])
+                                    
+            res.append(tmp_list)
+        
+        return res
