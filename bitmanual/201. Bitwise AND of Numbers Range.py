0d7f47825add370b39c3f2cec0e16329fe582ea1
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-06-20 16:01:30 -0700

    important failure

--- bitmanual/201. Bitwise AND of Numbers Range.py
+++ bitmanual/201. Bitwise AND of Numbers Range.py
@@ -18,19 +18,29 @@ It copies the bit if it is set in one operand but not both.
 
 >> Binary rightshift:
 
-
+why we use in this problem:
+to get the ith bit of 
 
 
 python teck: step back 
 for i in range(32)[::-1]:
-
-
-
 
 x = x | y  
 equals 
 x|=y
 
 
-
 """
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        ret = 0
+        for i in range(32)[::-1]:  # 31,30,29....0
+             # we compare from the highest bit 31th bit(32)
+            if m>>i != n>>i:  # if m's i bit not same with n's i bit
+              
+                break    # break  why: because they are not same otherwise keep m's i bit
+            ret |= (m>>i&1)<<i
+            # give ret the bit of m
+        return ret
+ # ref https://leetcode.com/problems/bitwise-and-of-numbers-range/discuss/56780/Short-1-liners-in-Python-with-explanation
+
