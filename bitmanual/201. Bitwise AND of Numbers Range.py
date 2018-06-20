98df4606cf65fd541feb266ed1836c7257a95181
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-06-20 15:27:21 -0700

    failed

--- bitmanual/201. Bitwise AND of Numbers Range.py
+++ bitmanual/201. Bitwise AND of Numbers Range.py
@@ -1,1 +1,36 @@
 """
+date:2018/06/20
+
+bit operation in python:
+
+Binary AND:
+(a & b) (means 0000 1100)
+Binary OR:
+(a | b) = 61 (means 0011 1101)
+
+^ Binary XOR:
+It copies the bit if it is set in one operand but not both.
+(a ^ b) = 49 (means 0011 0001)
+
+
+<< Binary leftshift:
+
+
+>> Binary rightshift:
+
+
+
+
+python teck: step back 
+for i in range(32)[::-1]:
+
+
+
+
+x = x | y  
+equals 
+x|=y
+
+
+
+"""
