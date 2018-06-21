aaf71ca0dfd03d6ea063fe051a7082a8fafd7587
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-06-20 17:03:14 -0700

    Update 136. Single Number.py

--- bitmanual/136. Single Number.py
+++ bitmanual/136. Single Number.py
@@ -20,7 +20,8 @@ we just know they are 2 and 1 is 1
 """
 
 
-## beat only 3%
+## my solution beat only 3%
+## use dictions as datastructure
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -38,6 +39,22 @@ class Solution(object):
         for unique_num in dict:
             if dict[unique_num]==1:
                 return unique_num
+            
+            
+ ## then I use set of python
+ ## and get the total sum of nums list
+ ## again only beats 10%
 class Solution(object):
     def singleNumber(self, nums):
-        
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        set_of_nums = set(nums)
+        sum_set = 0
+        sum_nums = 0
+        for set_num in set_of_nums:
+            sum_set += set_num
+        for num in nums:
+            sum_nums += num
+        return 2*sum_set - sum_nums
