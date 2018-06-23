09be3724bc0fa1fbfe7ae696ba2e811cb33aea19
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-06-22 22:55:56 -0700

    Update 21. Merge Two Sorted Lists.py

--- linedlist/21. Merge Two Sorted Lists.py
+++ linedlist/21. Merge Two Sorted Lists.py
@@ -5,20 +5,19 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        p1 = l1
         
-        p2 = l2
         
+
         root = q = ListNode(0)
         
-        while(p1 or p2):
-            if(p1.val>p2.val):
-                q.next = ListNode(p2.val)   # add p2 to q and 
-                p2 = p2.next   # p2 go to next node in l2
-   .            q = q.next
+        while(l1 or l2):
+            if(l1.val>l2.val):
+                q.next = ListNode(l2.val)   # add p2 to q and 
+                l2 = l2.next   # p2 go to next node in l2
+                q = q.next
             else:
-                q.next = ListNode(p1.val) # add p1 to q
-                p1 = p1.next
+                q.next = ListNode(l1.val) # add p1 to q
+                l1 = l1.next
                 q = q.next
         
         return root.next
