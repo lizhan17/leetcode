a5e5439fe8dffee11f8095b1335427e98f1e81c7
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-06-29 18:28:45 -0700

    Update 82. Remove Duplicates from Sorted List II.py

--- linedlist/82. Remove Duplicates from Sorted List II.py
+++ linedlist/82. Remove Duplicates from Sorted List II.py
@@ -15,3 +15,32 @@ class Solution(object):
         ## step through the node
         ## if we found the head.next val = head.val just find another head
         ## else we found the unique node add to the output
+
+        
+        
+ ## sol from others with my analyis
+## ref  https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28398/clean-python-solution-involving-dummy-node
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        dummy.next = head
+        prev, curr = dummy, dummy.next
+        
+        while curr:
+            ##
+            if curr.next and curr.val == curr.next.val:
+                val_to_rem = curr.val
+                
+                while curr and curr.val == val_to_rem:
+                    curr = curr.next
+                    
+                prev.next = curr
+                
+            else:
+                prev, curr = curr, curr.next
+                
+        return dummy.next
