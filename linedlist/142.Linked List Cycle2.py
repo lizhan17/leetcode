26406fb591ed36ab87a2907a88c1a3fd5e8100dc
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-07-11 09:42:46 +0800

    Update 142.Linked List Cycle2.py

--- linedlist/142.Linked List Cycle2.py
+++ linedlist/142.Linked List Cycle2.py
@@ -30,3 +30,70 @@ my first sol is just make a diction(hash map)
       
       
       
+# mythird sol
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+#  the  second solution is to check each node until we find
+# core logic is that a head node is its descentant node
+        cur = head
+        while(cur):
+            child = cur.next
+            while(child):
+                if(id(cur) == id(child)):
+                    return cur
+                else:
+                    child = child.next
+                    if child is not None:
+                        print("cur:")
+                        print(cur.val)
+                        print(child.val)
+                   
+            #end while  go to next pointer
+            cur = cur.next
+        #end while we didn't find such a special node
+        return None
+       
+     ## leetcode solution
+     class Solution(object):
+    def getIntersect(self, head):
+        tortoise = head
+        hare = head
+
+        # A fast pointer will either loop around a cycle and meet the slow
+        # pointer or reach the `null` at the end of a non-cyclic list.
+        while hare is not None and hare.next is not None:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return tortoise
+
+        return None
+
+    def detectCycle(self, head):
+        if head is None:
+            return None
+
+        # If there is a cycle, the fast/slow pointers will intersect at some
+        # node. Otherwise, there is no cycle, so we cannot find an entrance to
+        # a cycle.
+        intersect = self.getIntersect(head)
+        if intersect is None:
+            return None
+
+        # To find the entrance to the cycle, we have two pointers traverse at
+        # the same speed -- one from the front of the list, and the other from
+        # the point of intersection.
+        ptr1 = head
+        ptr2 = intersect
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        return ptr1
+
+      
