33ddc58db3942adbfb48a781882258e23a9f281c
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-07-18 07:43:17 +0800

    Update 147. Insertion Sort List.py

--- linedlist/147. Insertion Sort List.py
+++ linedlist/147. Insertion Sort List.py
@@ -1,3 +1,6 @@
+
+
+
 class Solution(object):
     def insertionSortList(self, head):
         """
@@ -5,21 +8,22 @@ class Solution(object):
         :rtype: ListNode
         """
         
-        if head.next is None:
+        if head is None or head.next is None:
             return head
     
         cur = head
-        
-        while cur.next:
+       
+        while cur and cur.next:
             if (cur.next.val >= cur.val):
                 cur = cur.next
             else:
                 if(head.val >= cur.next.val): # the todo element is smaller than the head of the ordered part
-                    # we just set the cur.next to be the head
+                    # we just set the cur.next to be the head,we don't need to change cur
                     tmp = cur.next
                     cur.next = cur.next.next
                     tmp.next = head
                     head = tmp
+                    
                 else: # the cur.next ele is bigger than the head and smalle than the tail of ordered part
                     
                     comparing_node = head
@@ -33,6 +37,19 @@ class Solution(object):
                     
                     prev.next = tmp2  # we use tmp2 to just insert one node is very important
                     tmp2.next = comparing_node
-            
-            cur = cur.next # or we just don't change cur.next pointer
+                    # we don't need to  change cur.next pointer
+        
         return head
+    
+    
+    
+    ### ref leetcode
+        def insertionSortList(self, head):
+        cur = dummy = ListNode(0)
+        while head:
+            if cur and cur.val > head.val: 
+                cur = dummy
+            while cur.next and cur.next.val < head.val: 
+                cur = cur.next
+            cur.next, cur.next.next, head = head, cur.next, head.next
+        return dummy.next
