a3ef0f32141005a5c599ab05c6cf08f29c2813b0
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-07-30 15:12:17 +0800

    wrong

--- array/26. Remove Duplicates from Sorted Array.py
+++ array/26. Remove Duplicates from Sorted Array.py
@@ -23,3 +23,14 @@ class Solution(object):
         
      # true solution is by 公瑾tm
      
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 
+        slow = fast = 0
+        while fast <= len(nums) - 1:
+            if nums[fast] != nums[slow]:
+                nums[slow+1] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow + 1
