1be23014691b764960d38d421c8e7a36942aa9d9
lizhan <32379931+lizhan17@users.noreply.github.com>
2018-08-02 18:03:59 +0800

    Create 110. Balanced Binary Tree.py

---
+++ tree/110. Balanced Binary Tree.py
@@ -0,0 +1,48 @@
+# ref https://leetcode.com/problems/balanced-binary-tree/discuss/35708/VERY-SIMPLE-Python-solutions-(iterative-and-recursive)-both-beat-90
+
+
+
+class Solution(object):
+    def isBalanced(self, root):
+            
+        def cla check(root):
+            if root is None:
+                return 0
+            left  = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+            
+        return check(root) != -1
+
+# 226 / 226 test cases passed.
+# Status: Accepted
+# Runtime: 80 ms
+
+
+
+class Solution(object):
+    def isBalanced(self, root):
+        stack, node, last, depths = [], root, None, {}
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack[-1]
+                if not node.right or last == node.right:
+                    node = stack.pop()
+                    left, right  = depths.get(node.left, 0), depths.get(node.right, 0)
+                    if abs(left - right) > 1: return False
+                    depths[node] = 1 + max(left, right)
+                    last = node
+                    node = None
+                else:
+                    node = node.right
+        return True
+
+
+# 226 / 226 test cases passed.
+# Status: Accepted
+# Runtime: 84 ms
